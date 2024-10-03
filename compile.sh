8ee32e845ad502a17f4699a6cdbc2546034daf69
Linus <linus.hasselkvist@gmail.com>
2024-10-03 17:29:26 +0200

    removed clutter

--- compile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gcc main.c -Wall chessboard.c moves.c ./pieces/bishop.c ./pieces/king.c ./pieces/knight.c ./pieces/pawn.c ./pieces/queen.c ./pieces/rook.c -o chess -I /VSCode/Terminal_Chess
